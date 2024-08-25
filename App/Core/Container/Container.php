8003a843dea911c1d54252c087aa0e763e1bac80
Neftali Acosta <neftaliacosta@outlook.com>
2024-08-25 01:36:46 -0600

    Fix(core): Rename core folders and add gitignore file

---
+++ App/Core/Container/Container.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Core\Container;
+
+use App\Core\Container\Exception\TableNotFoundException;
+use App\Core\coreException;
+
+/**
+ * container
+ * Saves the relationship between the original database and the model alias
+ *
+ * @author Neftalí Marciano Acosta <neftaliacosta@outlook.com>
+ * @copyright (c) 2024, Neftali Marciano Acosta
+ * @link https://www.linkedin.com/in/neftaliacosta
+ * @version 1.0
+ */
+class Container{
+
+    /**
+     * Alias tables dictionary
+     *
+     * @var array $tables
+     */
+    public static array $tables = array(
+        'entity' => 'alias_table',
+
+    );
+
+    /**
+     * Gets the actual name of the table in the database
+     *
+     * @param string $table
+     * @return string
+     */
+    public static function getTable(string $table): string
+    {
+        try {
+            if (array_key_exists($table, self::$tables)) {
+                return self::$tables[$table];
+            }else{
+                throw new TableNotFoundException('The alias table does not exist.');
+            }
+        } catch (coreException $e) {
+            echo $e->errorMessage();
+        }
+    }
+
+}
---
+++ App/Core/Container/Exception/TableNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Core\Container\Exception;
+
+use App\Core\CoreException;
+
+class TableNotFoundException extends CoreException
+{
+
+}
